995bc220a7418ff562679cd4e7c3dff7591b5ab2
unknown <mathapationkar8@gmail.com>
2021-07-12 10:56:32 +0530

    first upload

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+import 'semantic-ui-css/semantic.min.css';
+
+function App() { 
+  return( <div>Hello world</div>
+  );
+}
+
+export default App;
